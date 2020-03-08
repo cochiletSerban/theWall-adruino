242192668e7411febaadb0197b7d161edd098714
cochiletSerban <cochiletSerban@gmail.com>
2020-03-08 11:14:02 +0200

    integrated with wifi

--- D1miniCode/test.ino
+++ D1miniCode/test.ino
@@ -1,60 +1,54 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
+#include "SPISlave.h"
 
 char* ssid = "UPC9472151";
 char* password =  "zv5pbYVnrrf5";
-
+uint8_t songId = NULL;
 
 void setup() {
-
   Serial.begin(115200);
-  delay(4000);   //Delay needed before calling the WiFi.begin
-
+  delay(4000);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
 
   Serial.println("Connected to the WiFi network");
 
+  SPISlave.onData([](uint8_t * data, size_t len) {
+    Serial.printf("songId: %d\n", data[0]);
+    songId = data[0];
+  });
+
+  SPISlave.begin();
 }
 
 void loop() {
 
-// if(WiFi.status() == WL_CONNECTED){   //Check WiFi connection status
+  if (songId) {
+    HTTPClient http;
+    http.begin("http://192.168.0.66:4000/");
+    http.addHeader("Content-Type", "application/json");
 
-   HTTPClient http;
+    char buffer[20];
+    sprintf(buffer, "{\"song\":\"%d\"}", songId);
 
-   http.begin("http://192.168.0.66:4000/");  //Specify destination for HTTP request
-   http.addHeader("Content-Type", "application/json");             //Specify content-type header
-   char buffer[20];
-    sprintf(buffer, "{\"song\":\"%d\"}", rand() % (60 + 1 - 0) + 0);
-   int httpResponseCode = http.POST(buffer);   //Send the actual POST request
+    int httpResponseCode = http.POST(buffer);
 
-   if(httpResponseCode>0){
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println(httpResponseCode);
+      Serial.println(response);
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
+    }
 
-    String response = http.getString();                       //Get the response to the request
-
-    Serial.println(httpResponseCode);   //Print return code
-    Serial.println(response);           //Print request answer
-
-   }else{
-
-    Serial.print("Error on sending POST: ");
-    Serial.println(httpResponseCode);
-
-   }
-
-   http.end();  //Free resources
-
-//  }else{
-
-//     Serial.println("Error in WiFi connection");
-
-//  }
-
-  delay(2500);  //Send a request every 10 seconds
+    http.end();
+    songId = NULL;
+  }
 
 }
